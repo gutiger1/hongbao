e105bee62e7163646a9dfee108c7d48198a21894
gutiger1 <165143542+gutiger1@users.noreply.github.com>
2024-11-08 21:01:07 +0800

    Delete hongbao directory

--- hongbao/hb/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class HbConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hb'
-    verbose_name='管理系统'
-
-    def ready(self):
-        import hb.signals
