89797a0e4d645f2c38cea5717a0f608cd700da95
gutiger1 <165143542+gutiger1@users.noreply.github.com>
2024-11-08 21:12:41 +0800

    Delete dingdanbaob.top directory

--- dingdanbaob.top/check_user.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from hb.models import FirstLevelUser
-
-user = User.objects.get(id=18)
-first_level_user = FirstLevelUser.objects.filter(user=user).first()
-print(first_level_user)
--- dingdanbaob.top/hb/__init__.py
+++
@@ -1,5 +0,0 @@
-default_app_config = 'hb.apps.HbConfig'
-
-from hongbao.celery import app as celery_app
-
-__all__ = ['celery_app']
